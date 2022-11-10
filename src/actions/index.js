d42d57b29c217be93f8f679a7da3bf9eb241f0d9
Mohamed Rismi Rukshan <rukshanlancer@gmail.com>
2022-11-10 10:30:59 +0800

    lodash chain usage

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,20 +1,35 @@
 import jsonPlaceHolder from '../apis/JsonPlaceHolder';
 import _ from 'lodash';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+    // console.log(getState().posts);
+    // const userIds =  _.uniq(_.map(getState().posts, 'userId'));
+
+    // userIds.forEach(id => dispatch(fetchUser(id)));
+    _.chain(getState().posts)
+     .map('userId')
+     .uniq()
+     .forEach(id => dispatch(fetchUser(id)))
+     .value()
+
+    // console.log(userIds);
+}
+
 export const fetchPosts = () => async dispatch =>{
     const response = await jsonPlaceHolder.get('/posts');
     // console.log(response.data);
     dispatch({ type:'FETCH_POSTS', payload: response.data });
 }
 
-// export const fetchUser = (id)=>async dispatch => {
-//     const response = await jsonPlaceHolder.get(`/users/${id}`);
-//     dispatch({type:'FETCH_USER', payload: response.data});
-// }
-
-export const fetchUser = id => dispatch =>_fetchUser(id, dispatch);
-const _fetchUser = _.memoize(async(id, dispatch) => {
+export const fetchUser = (id)=>async dispatch => {
     const response = await jsonPlaceHolder.get(`/users/${id}`);
+    dispatch({type:'FETCH_USER', payload: response.data});
+}
 
-    dispatch({type: 'FETCH_USER', payload: response.data});
-}) ;
+// export const fetchUser = id => dispatch =>_fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async(id, dispatch) => {
+//     const response = await jsonPlaceHolder.get(`/users/${id}`);
+
+//     dispatch({type: 'FETCH_USER', payload: response.data});
+// }) ;
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import { fetchPosts } from '../actions';
+import { fetchPostsAndUsers } from '../actions';
 
 import UserHeader from './UserHeader';
 
 class PostList extends React.Component{
     componentDidMount(){
-        this.props.fetchPosts();
+        // this.props.fetchPosts();
+        this.props.fetchPostsAndUsers();
     }
     renderList(){
         return this.props.posts.map(post=>{
@@ -35,6 +36,6 @@ const mapStateToProps = (state) =>{
     return {posts: state.posts};
 }
 export default connect(
-    mapStateToProps, {fetchPosts}
+    mapStateToProps, {fetchPostsAndUsers}
 )(PostList);
 
